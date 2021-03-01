970cbd50e847b2aa2f7ed53360d9a6c7545bf961
yousefsaqoul <76205108+yousefsaqoul@users.noreply.github.com>
2021-03-01 14:24:15 +0100

    add

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -1,26 +1,35 @@
-
-
-
+// _________________________lev 2-5 table________________________________
 
 function generateTable() {
     let userInputZeilen = document.getElementById('zeilenFeld').value
     let userInputSpalten = document.getElementById('spaltenFeld').value
     let result = document.getElementById('result');
-
     let table = document.createElement('table');
     let tableBody = document.createElement('tbody');
     table.appendChild(tableBody);
 
-    for (var i = 0; i < userInputZeilen; i++) {
+    for (var i = 1; i <= userInputZeilen; i++) {
         let tr = document.createElement('tr');
         tableBody.appendChild(tr);
 
-        for (var j = 0; j < userInputSpalten; j++) {
+        for (var j = 1; j <= userInputSpalten; j++) {
             let td = document.createElement('TD');
             td.width = '75';
-            td.appendChild(document.createTextNode("  " + i + "," + j));
+            td.appendChild(document.createTextNode(i + "," + j));
             tr.appendChild(td);
         }
     }
     result.appendChild(table);
+}
+// _________________________lev 2-8 einkaufList________________________________
+
+let speicherButton = document.querySelector('input[type="submit"]');
+speicherButton.addEventListener('click', einkaufList);
+let userInput = document.getElementById('userinput')
+let ul = document.querySelector('ul')
+function einkaufList() {
+    let newList = document.createElement('li')
+    userInputResult = document.createTextNode(userInput.value)
+    newList.appendChild(userInputResult)
+    ul.appendChild(newList)
 }
