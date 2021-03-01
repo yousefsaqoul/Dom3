7c2bf0595c69c7bf4a861c30e78589612bad8910
yousefsaqoul <76205108+yousefsaqoul@users.noreply.github.com>
2021-03-01 12:37:57 +0100

    add

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -7,22 +7,16 @@ function generateTable() {
     let userInputSpalten = document.getElementById('spaltenFeld').value
     let result = document.getElementById('result');
 
-
-
-
-
-    let table = document.createElement('TABLE');
-    table.border = '1';
-
-    let tableBody = document.createElement('TBODY');
+    let table = document.createElement('table');
+    let tableBody = document.createElement('tbody');
     table.appendChild(tableBody);
 
     for (var i = 0; i < userInputZeilen; i++) {
-        var tr = document.createElement('TR');
+        let tr = document.createElement('tr');
         tableBody.appendChild(tr);
 
         for (var j = 0; j < userInputSpalten; j++) {
-            var td = document.createElement('TD');
+            let td = document.createElement('TD');
             td.width = '75';
             td.appendChild(document.createTextNode("  " + i + "," + j));
             tr.appendChild(td);
